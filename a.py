c8f25b16ec26a694c70715ccc981a554a9a47628
jinjin04280428 <31947688+jinjin04280428@users.noreply.github.com>
2023-07-15 01:39:44 +0000

    modify

---
+++ a.py
@@ -0,0 +1,21 @@
+import random
+min = 1
+max = 100
+count=0
+target = random.randint(min, max)
+print("====猜數字遊戲====\n\n")
+while True:
+    keyin=int(input(f'猜數字範團{min}~{max}:'))
+    count+=1          
+    if(keyin == target):
+       print(f"賓果!猜對了,答案是:{target}")
+       print(f"您共猜了{count}次")
+       break
+    elif keyin > target:  
+       print("再小一點")
+       max = keyin-1
+    elif keyin < target: 
+       print("再大一點")
+       min = keyin + 1
+       print(f"您已經猜了{count}次")
+print('遊戲結束')
